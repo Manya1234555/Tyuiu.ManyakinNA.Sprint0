50355af5924e4dfe5acba22893aa7f358e2d0cfc
Tyuiu.ManyakinNA@outlook.com <Tyuiu.ManyakinNA@outlook.com>
2024-09-19 21:47:11 +0500

    Add project files.

---
+++ Tyuiu.ManyakinNA.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.ManyakinNA.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
