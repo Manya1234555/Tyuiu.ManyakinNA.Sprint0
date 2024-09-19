d8b7e98e69493272f2d669620f8eb307ae94ad7f
Tyuiu.ManyakinNA@outlook.com <Tyuiu.ManyakinNA@outlook.com>
2024-09-19 23:19:32 +0500

    Task2

---
+++ Tyuiu.ManyakinNA.Sprint0.Task2.V0.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.ManyakinNA.Sprint0.Task2.V0.Lib
+{
+    public class DataService
+    {
+        public static string GetMessage(string name)
+
+        {
+            return $"Привет, {name}";
+        }
+    }
+
+}   
---
+++ Tyuiu.ManyakinNA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.ManyakinNA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ManyakinNA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(DataService.GetMessage("Никита"));
+            Console.ReadKey();
+        }
+
+    }
+}
