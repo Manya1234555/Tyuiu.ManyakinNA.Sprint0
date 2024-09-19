8a8b9e89a6f33c28346c61110544fd48352e3e0c
Tyuiu.ManyakinNA@outlook.com <Tyuiu.ManyakinNA@outlook.com>
2024-09-20 00:03:42 +0500

    Task4

---
+++ Tyuiu.ManyakinNA.Sprint0.Taks4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+using Tyuiu.ManyakinNA.Sprint0.Taks4.V0.Lib;
+
+
+namespace Tyuiu.ManyakinNA.Sprint0.Taks4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionalValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+       
+        }
+        [TestMethod]    
+        public void CheckedSubstractionValid()
+        {
+            Assert.AreEqual(5, DataService.Substraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckedMultiplicationlValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(5, 10));
+        }
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+
+    }
+
+
+}
---
+++ Tyuiu.ManyakinNA.Sprint0.Taks4.V0/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.ManyakinNA.Sprint0.Taks4.V0.Lib;
+
+
+namespace Tyuiu.ManyakinNA.Sprint0.Taks4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+
+            Console.WriteLine(DataService.Multiplication(10, 10));
+
+            Console.WriteLine(DataService.Substraction(15, 5));
+
+            Console.WriteLine(DataService.Division(5, 5));
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
