7de730316ee30cd636845e692af40f145618defd
Tyuiu.ManyakinNA@outlook.com <Tyuiu.ManyakinNA@outlook.com>
2024-09-19 21:56:47 +0500

    Task1

---
+++ Tyuiu.ManyakinNA.Sprint0.Taks1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.ManyakinNA.Sprint0.Taks1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Никита";
+            int age = 22;
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " года! ");
+            Console.ReadKey();
+        }
+    }
+}
